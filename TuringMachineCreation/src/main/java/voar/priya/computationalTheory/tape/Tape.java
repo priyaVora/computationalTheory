c9bb9784fa4ea8848621027d3a703a523f70eed8
priyaVora <prvora89@gmail.com>
2018-07-19 07:49:11 -0600

    Update

--- TuringMachineCreation/src/main/java/voar/priya/computationalTheory/tape/Tape.java
+++ TuringMachineCreation/src/main/java/voar/priya/computationalTheory/tape/Tape.java
@@ -3,10 +3,11 @@ package voar.priya.computationalTheory.tape;
 public class Tape {
 	private char[] tapeArray;
 	private int head = 226;
+	String equation;
 
 	public Tape(String equation) {
 		createTape(equation);
-		System.out.println("Print: ");
+		this.equation = equation;
 		printTape();
 	}
 
@@ -16,7 +17,6 @@ public class Tape {
 	}
 
 	private void createTape(String equation) {
-		System.out.println("Start");
 		tapeArray = new char[450 + equation.length()];
 		tapeArray[head] = '=';
 		char[] equationArray = equation.toCharArray();
@@ -25,10 +25,10 @@ public class Tape {
 		for (int i = 0; i < tapeArray.length; i++) {
 			if (i < head) {
 				tapeArray[i] = 'E';
-			} else if (i > head && i <= 232) {
+			} else if (i > head && i <= (head + equation.length())) {
 				tapeArray[i] = equationArray[counter];
 				counter++;
-			} else if(i != 226) { 
+			} else if (i != 226) {
 				tapeArray[i] = 'E';
 			}
 		}
@@ -46,7 +46,7 @@ public class Tape {
 	}
 
 	public void printTape() {
-		for (int i = 0; i < tapeArray.length; i++) {
+		for (int i = 110; i < (head + equation.length() + ((equation.length()))); i++) {
 			System.out.print(tapeArray[i]);
 		}
 	}
@@ -58,4 +58,21 @@ public class Tape {
 	private void shiftRight() {
 		head++;
 	}
+
+	public char[] getTapeArray() {
+		return tapeArray;
+	}
+
+	public void setTapeArray(char[] tapeArray) {
+		this.tapeArray = tapeArray;
+	}
+
+	public int getHead() {
+		return head;
+	}
+
+	public void setHead(int head) {
+		this.head = head;
+	}
+
 }
--- TuringMachineCreation/src/main/java/vora/priya/computationalTheory/TuringMachine/TuringMachine.java
+++ TuringMachineCreation/src/main/java/vora/priya/computationalTheory/TuringMachine/TuringMachine.java
@@ -1,22 +1,32 @@
 package vora.priya.computationalTheory.TuringMachine;
 
+import voar.priya.computationalTheory.tape.Tape;
+
 public class TuringMachine {
 	public State state;
+	Tape tape;
+	String equation;
 	private State_Start start;
 	private STATE_REDUCE_ONE reduceOne_State;
 	private STATE_REDUCE_E_Number reduce_E_Number;
 
-	public TuringMachine() {
+	public TuringMachine(String equation) {
 		this.setState(reduceOne_State);
-		setAllStates();
+		setAllStates_Tape();
 	}
 
-	public void setAllStates() { 
+	public void calculate() {
+
+	}
+
+	public void setAllStates_Tape() {
 		this.start = new State_Start(this);
 		this.reduce_E_Number = new STATE_REDUCE_E_Number(this);
-		this.reduceOne_State =  new STATE_REDUCE_ONE(this);
+		this.reduceOne_State = new STATE_REDUCE_ONE(this);
+		this.equation = equation;
+		this.tape = new Tape(equation);
 	}
-	
+
 	public State getState() {
 		return state;
 	}
